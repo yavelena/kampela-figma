6f8916509cb95dc31c9b9e03be65dddaabbd494c
Elena Yavorskaya <yavelena@gmail.com>
2024-02-27 00:33:20 +0200

    JS horizontal scrolling fixed, css fixes

--- 1920x/js/main.js
+++ 1920x/js/main.js
@@ -4,22 +4,39 @@ function CustomScroll(){
         e.preventDefault();
         e = window.event || e;
         let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        document.querySelector(class_name).scrollLeft -= (delta * 40);
+        this.scrollLeft -= (delta * 40);
     }
 
     var mobile_width = 1024;
-    var class_name = (window.innerWidth > mobile_width) ? ".scrollable-full-screen" : ".scrollable-mobile";
-    // alert('CustomScroll: ' + window.innerWidth + ' class name: ' + class_name);
-
-    document.querySelector(".scrollable-full-screen").removeEventListener("mousewheel", ScrollH, false);
-    document.querySelector(".scrollable-full-screen").removeEventListener("DOMMouseScroll", ScrollH, false);
-    document.querySelector(".scrollable-mobile").removeEventListener("mousewheel", ScrollH, false);
-    document.querySelector(".scrollable-mobile").removeEventListener("DOMMouseScroll", ScrollH, false);
+    const nodeList_fullscreen = document.querySelectorAll(".scrollable-full-screen");
+    const nodeList_mobile = document.querySelectorAll(".scrollable-mobile");
     
-    if (document.querySelector(class_name).addEventListener){
-        document.querySelector(class_name).addEventListener("mousewheel", ScrollH, false);
-        document.querySelector(class_name).addEventListener("DOMMouseScroll", ScrollH, false);
+    if (window.innerWidth > mobile_width){
+
+        for (let i = 0; i < nodeList_fullscreen.length; i++){
+            nodeList_fullscreen[i].addEventListener("mousewheel", ScrollH, false);
+            nodeList_fullscreen[i].addEventListener("DOMMouseScroll", ScrollH, false);
+        }
+
+        for (let i = 0; i < nodeList_mobile.length; i++){
+            nodeList_mobile[i].removeEventListener("mousewheel", ScrollH, false);
+            nodeList_mobile[i].removeEventListener("DOMMouseScroll", ScrollH, false);
+        }
+        
+    } 
+    else {
+
+        for (let i = 0; i < nodeList_fullscreen.length; i++){
+            nodeList_fullscreen[i].removeEventListener("mousewheel", ScrollH, false);
+            nodeList_fullscreen[i].removeEventListener("DOMMouseScroll", ScrollH, false);
+        }
+
+        for (let i = 0; i < nodeList_mobile.length; i++){
+            nodeList_mobile[i].addEventListener("mousewheel", ScrollH, false);
+            nodeList_mobile[i].addEventListener("DOMMouseScroll", ScrollH, false);
+        }
     }
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
