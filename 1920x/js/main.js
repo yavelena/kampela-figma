e773f05ec6f3e72d55e24dc82f17c82bed887899
Elena Yavorskaya <yavelena@gmail.com>
2024-03-09 11:57:14 -0500

    Added specification for full screen mode, added switching between horizontal and vertical scrolling

--- 1920x/js/main.js
+++ 1920x/js/main.js
@@ -1,57 +1,60 @@
 function CustomScroll(){
 
     function ScrollH(e){
-        e.preventDefault();
-        e = window.event || e;
-        let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        this.scrollLeft -= (delta * 40);
+        var mobile_width = 1024;
+        let scrollSpeed = 30;
+        if (window.innerWidth<mobile_width && !this.classList.contains("for-full-screen")
+        || window.innerWidth>=mobile_width && !this.classList.contains("for-mobile")){
+            if ( !e.deltaY ) return;
+            let scrollDirection = (e.deltaY > 0) ? 1 : -1;
+            let scrollLeft = Math.round(this.scrollLeft);
+            let maxScrollLeft = Math.round( this.scrollWidth - this.clientWidth );
+            // this.scrollLeft += e.deltaY;
+            this.scrollLeft += scrollSpeed * scrollDirection;
+            if( 
+                (scrollDirection === -1  && scrollLeft > 0) ||
+                (scrollDirection === 1 && scrollLeft < maxScrollLeft ) 
+              ) e.preventDefault()
+            // console.log(Math.round(this.scrollLeft) + "; " + maxScrollLeft);
+        } 
     }
 
-    var mobile_width = 1024;
-    const nodeList_fullscreen = document.querySelectorAll(".scrollable-full-screen");
-    const nodeList_mobile = document.querySelectorAll(".scrollable-mobile");
-    
-    if (window.innerWidth > mobile_width){
-
-        for (let i = 0; i < nodeList_fullscreen.length; i++){
-            nodeList_fullscreen[i].addEventListener("mousewheel", ScrollH, false);
-            nodeList_fullscreen[i].addEventListener("DOMMouseScroll", ScrollH, false);
-        }
-
-        for (let i = 0; i < nodeList_mobile.length; i++){
-            nodeList_mobile[i].removeEventListener("mousewheel", ScrollH, false);
-            nodeList_mobile[i].removeEventListener("DOMMouseScroll", ScrollH, false);
-        }
-        
-    } 
-    else {
-        // alert("mobile");
-
-        for (let i = 0; i < nodeList_fullscreen.length; i++){
-            nodeList_fullscreen[i].removeEventListener("mousewheel", ScrollH, false);
-            nodeList_fullscreen[i].removeEventListener("DOMMouseScroll", ScrollH, false);
-        }
-
-        for (let i = 0; i < nodeList_mobile.length; i++){
-            nodeList_mobile[i].addEventListener("mousewheel", ScrollH, false);
-            nodeList_mobile[i].addEventListener("DOMMouseScroll", ScrollH, false);
-        }
+    function addScrollHandler(item){
+        item.addEventListener("wheel", ScrollH, false);
+        item.addEventListener("DOMMouseScroll", ScrollH, false);
     }
-
+    document.querySelectorAll(".scrollable").forEach(addScrollHandler);
 }
 
+function ShowSpec(){
+    var nodeList_showspec = document.getElementsByClassName("show-spec");
+    var nodeList_hidespec = document.querySelectorAll(".product .pop-up-close-btn")
+    var price_overlay = document.getElementById("price-overlay");
+    if (price_overlay){
+        for (let i=0; i<nodeList_showspec.length; i++){
+            nodeList_showspec[i].onclick = function() {
+                price_overlay.style.display = "block";
+            }
+        }
+
+        for (let i=0; i<nodeList_hidespec.length; i++){
+            nodeList_hidespec[i].onclick = function() {
+                price_overlay.style.display = "none";
+            }
+        }
+
+        price_overlay.onclick = function(){
+            price_overlay.style.display = "none";
+        }
+    }
+    
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    CustomScroll()
-}); 
-
-
-window.addEventListener('resize', function(event) {
-    // alert('New size: ' + window.innerWidth);
     CustomScroll();
-}, true);
-
-
-
+    ShowSpec();
+}); 
 
 
 
